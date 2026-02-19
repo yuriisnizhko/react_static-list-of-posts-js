267f9588e87ebe6aa9a2ca430911e6e88b37a215
yuriisnizhko <yuriisnizhko@gmail.com>
2026-02-19 23:15:34 +0200

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import './App.scss';
-
 import postsFromServer from './api/posts.json';
 import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
@@ -7,13 +5,17 @@ import usersFromServer from './api/users.json';
 import { PostList } from './components/PostList';
 
 function getUserById(userId) {
-  return usersFromServer.find(user => user.id === userId) || null;
+  return (
+    usersFromServer.find(user => user.id === userId) || {
+      id: 0,
+      name: 'Unknown author',
+      email: '',
+    }
+  );
 }
 
 function getCommentsByPostId(postId) {
-  return (
-    commentsFromServer.filter(comment => comment.postId === postId) || null
-  );
+  return commentsFromServer.filter(comment => comment.postId === postId);
 }
 
 export const posts = postsFromServer.map(post => ({
